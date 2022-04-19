1b9507781f74b8cb97efcfb48f57dd098d183c18
taengbae <gudqo04@gmail.com>
2022-04-19 16:11:54 +0900

    [태형배] 커뮤니티 글쓰는 화면 및 댓글 창 생성

--- app/src/main/java/com/example/front_sunhan/View/adapter/CommunityAdapter.java
+++ app/src/main/java/com/example/front_sunhan/View/adapter/CommunityAdapter.java
@@ -1,73 +1,106 @@
-package com.example.front_sunhan.View.adapter;
-
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.front_sunhan.Model.CommunityItem;
-import com.example.front_sunhan.R;
-
-import java.util.ArrayList;
-
-public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.ViewHolder> {
-
-    private ArrayList<CommunityItem> CommunityList;
-
-    @NonNull
-    @Override
-    public CommunityAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.community_item, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull CommunityAdapter.ViewHolder holder, int position) {
-        holder.onBind(CommunityList.get(position));
-    }
-
-    public void setCommunityList(ArrayList<CommunityItem> list){
-        this.CommunityList = list;
-        notifyDataSetChanged();
-    }
-
-    @Override
-    public int getItemCount() {
-        return CommunityList.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView profile;
-        TextView name;
-        TextView uptime;
-        TextView content;
-        TextView count;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            profile = (ImageView) itemView.findViewById(R.id.userprofile);
-            name = (TextView) itemView.findViewById(R.id.userid);
-            uptime = (TextView) itemView.findViewById(R.id.uploadtime);
-            content = (TextView) itemView.findViewById(R.id.content);
-            count = (TextView) itemView.findViewById(R.id.commentnum);
-        }
-
-        void onBind(CommunityItem item){
-            profile.setImageResource(item.getProfile());
-            name.setText(item.getNickname());
-            uptime.setText(item.getWritetime());
-            content.setText(item.getContent());
-            count.setText(item.getWritenum());
-        }
-    }
-
-
-}
-
+//package com.example.front_sunhan.View.adapter;
+//
+//import android.content.Context;
+//import android.graphics.drawable.ShapeDrawable;
+//import android.graphics.drawable.shapes.OvalShape;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.example.front_sunhan.Model.CommunityItem;
+//import com.example.front_sunhan.Model.MypageItem;
+//import com.example.front_sunhan.R;
+//import com.example.front_sunhan.View.interfaceListener.OnClickMyPageItemListener;
+//
+//import java.util.ArrayList;
+//
+//public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.ViewHolder> { {
+//    private Context context;
+//    ArrayList<MypageItem> communityItemArrayList;
+//
+//    public CommunityAdapter(Context context, ArrayList<CommunityItem> arrayList){
+//            this.context = context ;
+//            this.communityItemArrayList= arrayList;
+//        }
+//
+//    @NonNull
+//    @Override
+//    public CommunityAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+//        View itemView = layoutInflater.inflate(R.layout.community_item, parent, false);
+//        return new CommunityAdapter.ViewHolder(itemView,this);
+//    }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull CommunityAdapter.ViewHolder holder, int position) {
+//            MypageItem item =mypageItemArrayList.get(position);
+////        holder.imageView.setImageResource(mypageItemArrayList.get(position).image);
+//            holder.textView.setText(mypageItemArrayList.get(position).getItemName());
+//        }
+//
+//        public void setOnClickCommunityItemListener(OnClickMyPageItemListener listener){
+//
+//            this.listener = listener;
+//        }
+//        @Override
+//        public void onItemClick(CommunityAdapter.ViewHolder holder, View view, int position) {
+//            if(listener != null){
+//                listener.onItemClick(holder,view,position);
+//            }
+//        }
+//
+//
+//        @Override
+//        public int getItemCount() {
+//            return communityItemArrayList.size();
+//        }
+//
+//        public static class ViewHolder extends RecyclerView.ViewHolder{
+//            TextView textView;
+//            ImageView imageView;
+//
+//            public ViewHolder(@NonNull View itemView , final OnClickMyPageItemListener listener) {
+//                super(itemView);
+////            imageView = itemView.findViewById(R.id.view_item);
+//                textView = itemView.findViewById(R.id.mypage_item);
+//
+//                itemView.setClickable(true);
+//                itemView.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        int position = getAdapterPosition();
+//
+//                        if(listener != null){
+//                            listener.onItemClick(MypageAdapter.ViewHolder.this, view, position);
+//                        }
+//                    }
+//                });
+//
+//
+//            }
+//
+//        }
+//
+//        public void addItem(CommunityItem item){
+//            communityItemArrayList.add(item);
+//        }
+//        public void setarrayList(ArrayList<CommunityItem> arrayList) {
+//
+//            this.communityItemArrayList = arrayList;
+//        }
+//
+//        public CommunityItem getItem(int position) {
+//
+//            return communityItemArrayList.get(position);
+//        }
+//
+//        public void setItem(int position, CommunityItem item) {
+//
+//            communityItemArrayList.set(position, item);
+//        }
+//    }
--- app/src/main/java/com/example/front_sunhan/View/fragment/CommunityFragment.java
+++ app/src/main/java/com/example/front_sunhan/View/fragment/CommunityFragment.java
@@ -1,9 +1,11 @@
 package com.example.front_sunhan.View.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,10 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.front_sunhan.R;
+import com.example.front_sunhan.View.activity.EditProfileActivity;
 import com.example.front_sunhan.View.activity.LoginActivity;
 
 public class CommunityFragment extends Fragment {
-    public void onCreate(Bundle savedInstanceState) {
+    RecyclerView communityRecyclerView;
+
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
     }
 
@@ -25,6 +30,18 @@ public class CommunityFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_community, null);
+
+        setRecyclerview(view);
+
         return view;
+    }
+
+    void setRecyclerview(View view){
+        communityRecyclerView = view.findViewById(R.id.communityrecyleView);
+        communityRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager recyclerViewManager = new LinearLayoutManager(getActivity());
+        communityRecyclerView.setLayoutManager(recyclerViewManager);
+        communityRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
     }
 }
